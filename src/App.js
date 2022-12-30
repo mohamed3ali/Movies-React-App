14e6f025b24544c162f21f86c8e74233d297ed1c
mohamed3ali <moali8938@gmail.com>
2022-12-30 21:16:20 +0200

    Movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Component/Home/Home';
+import Layout from './Component/Layout/Layout';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom';
+import Movies from './Component/Movies/Movies';
+import Login from './Component/Login/Login';
+import Register from './Component/Register/Register';
+import People from './Component/People/People';
+import Tv from './Component/Tv/Tv';
+import jwtDecode from 'jwt-decode';
+import { useEffect, useState } from 'react';
+import Profile from './Component/Profile/Profile';
+import ProtectedRoute from './Component/ProtectedRoute/ProtectedRoute';
+import ItemDetails from './Component/ItemDetails/ItemDetails';
+import { Offline } from 'react-detect-offline';
+import Search from './Component/Search/search';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() 
+{
+  useEffect(()=>{
+    if(localStorage.getItem('userToken') !==null)
+    {
+      saveUserData();
+    }
+  } , [])
+  const [userData, setuserDate] = useState(null);
+function saveUserData(){
+  let encodedToken = localStorage.getItem('userToken');
+ let decodedToken = jwtDecode(encodedToken);
+console.log(decodedToken);
+ setuserDate(decodedToken)
+
 }
 
+  let routers = createBrowserRouter([
+    {path:'/', element:<Layout setuserDate={setuserDate} userData={userData}/> , children:[
+      {path:'home' , element:<ProtectedRoute userData={userData}><Home/></ProtectedRoute>},
+      {path:'movies' , element:<ProtectedRoute userData={userData}><Movies/></ProtectedRoute>},
+      {path:'profile' , element:<ProtectedRoute userData={userData}><Profile userData={userData}/></ProtectedRoute>},
+      {path:'login' , element:<Login saveUserData={saveUserData}/>},
+      {index:true, element:<Register/>},
+      {path:'tv' , element:<ProtectedRoute userData={userData}><Tv/></ProtectedRoute>},
+      {path:'search' , element:<ProtectedRoute userData={userData}><Search/></ProtectedRoute>},
+      {path:'people' , element:<ProtectedRoute userData={userData}><People/></ProtectedRoute>},
+      {path:'itemdetails/:id/:media_Type' , element:<ProtectedRoute userData={userData}><ItemDetails/></ProtectedRoute>},
+    ]}
+  ])
+    return<>
+      <div>
+ 
+    <Offline> <div className='offline'> You are offline (surprise!)</div></Offline>
+  </div>
+  <RouterProvider router={routers}/>
+    </> 
+  }
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
+import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
